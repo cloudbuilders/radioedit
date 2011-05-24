f8407d9f183b035d37bc1203dd45acfe2d8fce10
Will Kelly <the.william.kelly@gmail.com>
2011-05-24 12:19:06 -0500

    * Added new_instance for creating a new nova instance (nova part does not work) * Added reset to destroy all running nova instances

--- radioedit.py
+++ radioedit.py
@@ -2,6 +2,7 @@
 
 import json
 import openstack.compute
+from uuid import uuid4
 from ConfigParser import SafeConfigParser
 
 
@@ -15,6 +16,19 @@ class Root(object):
         self.user = cp.get("rackspacecloud", "user")
         self.apikey = cp.get("rackspacecloud", "apikey")
         self.compute = Compute(username=self.user, apikey=self.apikey)
+        self.prefix = cp.get("radioedit", "prefix")
+
+    def new_instance(self):
+        img = [x for x in self.compute.images.list()
+                if x.name.find("Ubuntu 10.10") != -1][0]
+        flav = [x for x in self.compute.flavors.list() if x.ram == 512][0]
+        name = self.prefix + str(uuid4())
+        return self.compute.servers.create(name, img.id, flav.id)
+
+    def reset(self):
+        for server in self.compute.servers.list():
+            if server.name.find(self.prefix) == 0:
+                self.compute.servers.delete(server)
 
     def list_instances(self):
         return
@@ -29,3 +43,4 @@ if __name__ == "__main__":
 
     root = Root(options.config_file)
     print root.user, root.apikey, root.compute
+    print root.compute.servers.list()
