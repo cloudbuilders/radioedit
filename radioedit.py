18008272a0da2259dc0ae2947404532188dd21dc
Anthony Young <sleepsonthefloor@gmail.com>
2011-07-19 20:53:14 -0700

    support multiple versions of radioedit

--- radioedit.py
+++ radioedit.py
@@ -110,7 +110,7 @@ class RadioEdit(object):
         return jsontemplate.expand(tmpl, {'log': log, 'host': host})
 
     @cherrypy.expose
-    def new(self, name=None, srvtype=None):
+    def new(self, name=None, srvtype=None, script='nova.sh'):
         password = self.gen_password()
         img = [i for i in self.compute.images.list()
                 if i.name.find("Ubuntu 10.10") != -1][0]
@@ -124,6 +124,7 @@ class RadioEdit(object):
             password=password,
             pubkey=self.pubkey,
             srvtype=srvtype,
+            script=script,
             )
         self.compute.servers.create(srvname, img.id, flav.id,
             files={"/etc/cron.d/firstboot": cron,
